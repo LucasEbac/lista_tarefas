026f594445a2bf72e55ec42f3bc5fbf1facc81f2
Lucas santos <Lucass.santos2015@outlook.com>
2023-08-24 12:48:53 -0300

    realizando teste

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ $(document).ready(function(){
         $('form').slideDown();
 
 })
-
-})
 $('form').on("submit", function(e){
     e.preventDefault();
-    const novaTarefa = $('form-tarefa').val();
+    const novaTarefa = $('nome-da-tarefa').val();
+    const novoItem = $('<li></li>');
+    $(`<li> ${novaTarefa}</li>`).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+
+
+$('li').click(function(){
+    $(this).addClass('marcado');
+});
+$('#novaTarefa').val("");
+})
 })
